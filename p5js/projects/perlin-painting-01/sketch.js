4ce3660c65f27667cb569529506e67a1ac74d967
Wiklow <contact@michaeldean.ca>
2019-07-14 04:38:43 +0100

    cleaned up code on perlin-painting and add image output

--- p5js/projects/perlin-painting-01/sketch.js
+++ p5js/projects/perlin-painting-01/sketch.js
@@ -1,7 +1,7 @@
 
 var numPoints = 45;
 var nStart = 0;
-var ampDist = 15;
+var ampDist = 14;
 var thickness = 0.05;
 var ampCurve;
 
@@ -14,28 +14,38 @@ function setup() {
   // Line quality
   strokeWeight(thickness);
   strokeJoin(ROUND);
+  noFill();
 
   ampCurve = height/8;
 }
 
 function draw() {
 
+  // Finish drawing after count goes to 8
+  if(nStart>8) {
+    return;
+  }
+
+  // Update animation, mostly the noise function
+  nStart += 0.01;
+
   // Line color
   //stroke(0,130,164);
 
   // Space between each point
   hop = width/numPoints;
-  noFill();
 
-  nStart += 0.01;
+  // Displace the line on the y axis
   var yDistort = noise(nStart)-0.5;
 
   // Draw line
   beginShape();
+
   curveVertex(0, height/2);
 
-  for (var i = 0; i < width; i=i+hop) {
+  for (var i = 0; i <= width; i=i+hop) {
     curveVertex(i, noise(i,nStart)*ampCurve*yDistort*ampDist+height/2);
   }
+  
   endShape();
 }
